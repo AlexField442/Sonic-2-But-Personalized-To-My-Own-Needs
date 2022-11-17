938d132257105af7d18c4060a3890edf70d0ab9a
AlexField290 <alexfield290@gmail.com>
2022-11-17 22:23:30 +1100

    Further increased index size limit
    Now we can basically store all the layout data (object, ring, physical) where-ever we want due to now using direct pointers.

--- s2.asm
+++ s2.asm
@@ -18687,195 +18687,10 @@ loadLevelLayout:
 	ror.b	#1,d0
 	lsr.w	#5,d0
 	lea	(Off_Level).l,a0
-	move.l	(a0,d0.w),d0
-	lea	(a0,d0.l),a0
+	movea.l	(a0,d0.w),a0
 	lea	(Level_Layout).w,a1
 	jmpto	KosDec, JmpTo_KosDec
 ; End of function loadLevelLayout
-
-; ===========================================================================
-
-;loadLevelLayout_Sonic1:
-	; This loads level layout data in Sonic 1's format. Curiously, this
-	; function has been changed since Sonic 1: in particular, it repeats
-	; the rows of the source data to fill the rows of the destination
-	; data, which provides some explanation for why so many of Sonic 2's
-	; backgrounds are repeated in their layout data. This repeating is
-	; needed to prevent Hidden Palace Zone's background from disappearing
-	; when the player moves to the left.
-
-	; Clear layout data.
-	lea	(Level_Layout).w,a3
-	move.w	#bytesToLcnt(Level_Layout_End-Level_Layout),d1
-	moveq	#0,d0
--	move.l	d0,(a3)+
-	dbf	d1,-
-
-	; The rows of the foreground and background layouts are interleaved
-	; in memory. This is done here:
-	lea	(Level_Layout).w,a3	; Foreground.
-	moveq	#0,d1			; Index into 'Off_Level' to get level foreground layout.
-	bsr.w	.loadLayout
-	lea	(Level_Layout+$80).w,a3	; Background.
-	moveq	#2,d1			; Index into 'Off_Level' to get level background layout.
-
-; ||||||||||||||| S U B R O U T I N E |||||||||||||||||||||||||||||||||||||||
-
-; sub_E4A2:
-.loadLayout:
-	; This expects 'Off_Level' to be in the format that it was in
-	; Sonic 1.
-	moveq	#0,d0
-	move.w	(Current_ZoneAndAct).w,d0
-	ror.b	#1,d0
-	lsr.w	#5,d0
-	add.w	d1,d0
-	lea	(Off_Level).l,a1
-	move.w	(a1,d0.w),d0
-	lea	(a1,d0.l),a1
-
-	moveq	#0,d1
-	move.w	d1,d2
-	move.b	(a1)+,d1	; Layout width.
-	move.b	(a1)+,d2	; Layout height.
-	move.l	d1,d5
-	addq.l	#1,d5
-	moveq	#0,d3
-	move.w	#$80,d3	; Size of layout row in memory.
-	divu.w	d5,d3	; Get how many times to repeat the source row to fill the destination row.
-	subq.w	#1,d3	; Turn into loop counter.
-
-.nextRow:
-	movea.l	a3,a0
-
-	move.w	d3,d4
-.repeatRow:
-	move.l	a1,-(sp)
-
-	move.w	d1,d0
-.nextByte:
-	move.b	(a1)+,(a0)+
-	dbf	d0,.nextByte
-
-	movea.l	(sp)+,a1
-	dbf	d4,.repeatRow
-
-	lea	(a1,d5.w),a1	; Next row in source data.
-	lea	$100(a3),a3	; Next row in destination data.
-	dbf	d2,.nextRow
-
-	rts
-; End of function .loadLayout
-
-; ===========================================================================
-
-;ConvertChunksFrom256x256To128x128:
-	; This converts Sonic 1-style 256x256 chunks to Sonic 2-style 128x128
-	; chunks.
-
-	; Destination of 128x128 chunks.
-	lea	($FE0000).l,a1
-	lea	($FE0000+8*8*2).l,a2
-	; Source of 256x256 chunks.
-	lea	(Chunk_Table).l,a3
-
-	move.w	#64-1,d1	; Process 64 256x256 chunks.
--	bsr.w	ConvertHalfOf256x256ChunkToTwo128x128Chunks
-	bsr.w	ConvertHalfOf256x256ChunkToTwo128x128Chunks
-	dbf	d1,-
-
-	lea	($FE0000).l,a1
-	lea	($FF0000).l,a2
-
-	; Insert a blank chunk at the start of chunk table.
-	move.w	#bytesToWcnt(8*8*2),d1
--	move.w	#0,(a2)+
-	dbf	d1,-
-
-	; Copy the actual chunks to after this blank chunk.
-	move.w	#bytesToWcnt($8000-(8*8*2)),d1
--	move.w	(a1)+,(a2)+
-	dbf	d1,-
-
-	rts
-; ===========================================================================
-
-;EliminateChunkDuplicates:
-	; This is a chunk de-duplicator.
-
-	; Copy first chunk into 'Chunk_Table'.
-	lea	($FE0000).l,a1
-	lea	(Chunk_Table).l,a3
-
-	moveq	#bytesToLcnt(8*8*2),d0
--	move.l	(a1)+,(a3)+
-	dbf	d0,-
-
-	moveq	#0,d7	; This holds how many chunks have been copied minus 1.
-	lea	($FE0000).l,a1
-	move.w	#$100-1,d5	; $100 chunks
-;loc_E55A:
-.nextChunk:
-	lea	(Chunk_Table).l,a3
-	move.w	d7,d6
-
-.doNextComparison:
-	movem.l	a1-a3,-(sp)
-
-	; Compare chunks.
-	move.w	#bytesToWcnt(8*8*2),d0
--	cmpm.w	(a1)+,(a3)+
-	bne.s	+
-	dbf	d0,-
-
-	; The chunks match.
-	movem.l	(sp)+,a1-a3
-	adda.w	#8*8*2,a1
-	dbf	d5,.nextChunk
-
-	bra.s	++
-; ===========================================================================
-+
-	; No match: check the next chunk.
-	movem.l	(sp)+,a1-a3
-	adda.w	#8*8*2,a3
-	dbf	d6,.doNextComparison
-
-	; Not a single match.
-
-	; Add this chunk to the output.
-	moveq	#bytesToLcnt(8*8*2),d0
--	move.l	(a1)+,(a3)+
-	dbf	d0,-
-
-	addq.l	#1,d7	; One more chunk has been added.
-	dbf	d5,.nextChunk
-/
-	bra.s	-	; infinite loop
-
-; ||||||||||||||| S U B R O U T I N E |||||||||||||||||||||||||||||||||||||||
-
-; sub_E59C:
-ConvertHalfOf256x256ChunkToTwo128x128Chunks:
-	moveq	#8-1,d0	 ; 8 rows.
--
-	; Do a row of chunk 1 (a chunk is 8 blocks wide and tall).
-	move.l	(a3)+,(a1)+
-	move.l	(a3)+,(a1)+
-	move.l	(a3)+,(a1)+
-	move.l	(a3)+,(a1)+
-	; Do a row of chunk 2.
-	move.l	(a3)+,(a2)+
-	move.l	(a3)+,(a2)+
-	move.l	(a3)+,(a2)+
-	move.l	(a3)+,(a2)+
-	dbf	d0,-
-
-	adda.w	#8*8*2,a1
-	adda.w	#8*8*2,a2
-
-	rts
-; End of function ConvertHalfOf256x256ChunkToTwo128x128Chunks
 
 ; ===========================================================================
 
@@ -29334,8 +29149,7 @@ RingsManager_Setup:
 	ror.b	#1,d0
 	lsr.w	#5,d0
 	lea	(Off_Rings).l,a1
-	move.l	(a1,d0.w),d0
-	lea	(a1,d0.w),a1
+	movea.l	(a1,d0.w),a1
 	lea	(Ring_Positions+6).w,a2	; first ring is left blank
 ; loc_172E0:
 RingsMgr_NextRowOrCol:
@@ -30011,8 +29825,7 @@ ObjectsManager_Init:
 	ror.b	#1,d0			; then this yields $0F80...
 	lsr.w	#5,d0			; and this yields $003E.
 	lea	(Off_Objects).l,a0	; Next, we load the first pointer in the object layout list pointer index,
-	movea.l	a0,a1			; then copy it for quicker use later.
-	adda.l	(a0,d0.w),a0		; (Point1 * 2) + $003E
+	movea.l	(a0,d0.w),a0		; then copy it for quicker use later.
 	; initialize each object load address with the first object in the layout
 	move.l	a0,(Obj_load_addr_right).w
 	move.l	a0,(Obj_load_addr_left).w
@@ -86482,41 +86295,41 @@ ColP_Invalid:
 ; Two entries per zone, pointing to the level layouts for acts 1 and 2 of each zone
 ; respectively.
 ;---------------------------------------------------------------------------------------
-Off_Level: zoneOrderedOffsetTable 4,2
-	zoneOffsetTableEntry.l Level_EHZ1
-	zoneOffsetTableEntry.l Level_EHZ2	; 1
-	zoneOffsetTableEntry.l Level_EHZ1	; 2
-	zoneOffsetTableEntry.l Level_EHZ1	; 3
-	zoneOffsetTableEntry.l Level_EHZ1	; 4
-	zoneOffsetTableEntry.l Level_EHZ1	; 5
-	zoneOffsetTableEntry.l Level_EHZ1	; 6
-	zoneOffsetTableEntry.l Level_EHZ1	; 7
-	zoneOffsetTableEntry.l Level_MTZ1	; 8
-	zoneOffsetTableEntry.l Level_MTZ2	; 9
-	zoneOffsetTableEntry.l Level_MTZ3	; 10
-	zoneOffsetTableEntry.l Level_MTZ3	; 11
-	zoneOffsetTableEntry.l Level_WFZ	; 12
-	zoneOffsetTableEntry.l Level_WFZ	; 13
-	zoneOffsetTableEntry.l Level_HTZ1	; 14
-	zoneOffsetTableEntry.l Level_HTZ2	; 15
-	zoneOffsetTableEntry.l Level_HPZ1	; 16
-	zoneOffsetTableEntry.l Level_HPZ1	; 17
-	zoneOffsetTableEntry.l Level_EHZ1	; 18
-	zoneOffsetTableEntry.l Level_EHZ1	; 19
-	zoneOffsetTableEntry.l Level_OOZ1	; 20
-	zoneOffsetTableEntry.l Level_OOZ2	; 21
-	zoneOffsetTableEntry.l Level_MCZ1	; 22
-	zoneOffsetTableEntry.l Level_MCZ2	; 23
-	zoneOffsetTableEntry.l Level_CNZ1	; 24
-	zoneOffsetTableEntry.l Level_CNZ2	; 25
-	zoneOffsetTableEntry.l Level_CPZ1	; 26
-	zoneOffsetTableEntry.l Level_CPZ2	; 27
-	zoneOffsetTableEntry.l Level_DEZ	; 28
-	zoneOffsetTableEntry.l Level_DEZ	; 29
-	zoneOffsetTableEntry.l Level_ARZ1	; 30
-	zoneOffsetTableEntry.l Level_ARZ2	; 31
-	zoneOffsetTableEntry.l Level_SCZ	; 32
-	zoneOffsetTableEntry.l Level_SCZ	; 33
+Off_Level: zoneOrderedTable 4,2
+	zoneTableEntry.l Level_EHZ1
+	zoneTableEntry.l Level_EHZ2	; 1
+	zoneTableEntry.l Level_EHZ1	; 2
+	zoneTableEntry.l Level_EHZ1	; 3
+	zoneTableEntry.l Level_EHZ1	; 4
+	zoneTableEntry.l Level_EHZ1	; 5
+	zoneTableEntry.l Level_EHZ1	; 6
+	zoneTableEntry.l Level_EHZ1	; 7
+	zoneTableEntry.l Level_MTZ1	; 8
+	zoneTableEntry.l Level_MTZ2	; 9
+	zoneTableEntry.l Level_MTZ3	; 10
+	zoneTableEntry.l Level_MTZ3	; 11
+	zoneTableEntry.l Level_WFZ	; 12
+	zoneTableEntry.l Level_WFZ	; 13
+	zoneTableEntry.l Level_HTZ1	; 14
+	zoneTableEntry.l Level_HTZ2	; 15
+	zoneTableEntry.l Level_HPZ1	; 16
+	zoneTableEntry.l Level_HPZ1	; 17
+	zoneTableEntry.l Level_EHZ1	; 18
+	zoneTableEntry.l Level_EHZ1	; 19
+	zoneTableEntry.l Level_OOZ1	; 20
+	zoneTableEntry.l Level_OOZ2	; 21
+	zoneTableEntry.l Level_MCZ1	; 22
+	zoneTableEntry.l Level_MCZ2	; 23
+	zoneTableEntry.l Level_CNZ1	; 24
+	zoneTableEntry.l Level_CNZ2	; 25
+	zoneTableEntry.l Level_CPZ1	; 26
+	zoneTableEntry.l Level_CPZ2	; 27
+	zoneTableEntry.l Level_DEZ	; 28
+	zoneTableEntry.l Level_DEZ	; 29
+	zoneTableEntry.l Level_ARZ1	; 30
+	zoneTableEntry.l Level_ARZ2	; 31
+	zoneTableEntry.l Level_SCZ	; 32
+	zoneTableEntry.l Level_SCZ	; 33
     zoneTableEnd
 ;---------------------------------------------------------------------------------------
 ; EHZ act 1 level layout (Kosinski compression)
@@ -88120,43 +87933,44 @@ MiscKoz_SpecialObjectLocations:	BINCLUDE	"misc/Special stage object location lis
 ;  The first commented number on each line is an array index; the second is the
 ;  associated zone.
 ;--------------------------------------------------------------------------------------
-Off_Rings: zoneOrderedOffsetTable 4,2
-	zoneOffsetTableEntry.l  Rings_EHZ_1	; 0  $00
-	zoneOffsetTableEntry.l  Rings_EHZ_2	; 1
-	zoneOffsetTableEntry.l  Rings_Lev1_1	; 2  $01
-	zoneOffsetTableEntry.l  Rings_Lev1_2	; 3
-	zoneOffsetTableEntry.l  Rings_Lev2_1	; 4  $02
-	zoneOffsetTableEntry.l  Rings_Lev2_2	; 5
-	zoneOffsetTableEntry.l  Rings_Lev3_1	; 6  $03
-	zoneOffsetTableEntry.l  Rings_Lev3_2	; 7
-	zoneOffsetTableEntry.l  Rings_MTZ_1	; 8  $04
-	zoneOffsetTableEntry.l  Rings_MTZ_2	; 9
-	zoneOffsetTableEntry.l  Rings_MTZ_3	; 10 $05
-	zoneOffsetTableEntry.l  Rings_MTZ_4	; 11
-	zoneOffsetTableEntry.l  Rings_WFZ_1	; 12 $06
-	zoneOffsetTableEntry.l  Rings_WFZ_2	; 13
-	zoneOffsetTableEntry.l  Rings_HTZ_1	; 14 $07
-	zoneOffsetTableEntry.l  Rings_HTZ_2	; 15
-	zoneOffsetTableEntry.l  Rings_HPZ_1	; 16 $08
-	zoneOffsetTableEntry.l  Rings_HPZ_2	; 17
-	zoneOffsetTableEntry.l  Rings_Lev9_1	; 18 $09
-	zoneOffsetTableEntry.l  Rings_Lev9_2	; 19
-	zoneOffsetTableEntry.l  Rings_OOZ_1	; 20 $0A
-	zoneOffsetTableEntry.l  Rings_OOZ_2	; 21
-	zoneOffsetTableEntry.l  Rings_MCZ_1	; 22 $0B
-	zoneOffsetTableEntry.l  Rings_MCZ_2	; 23
-	zoneOffsetTableEntry.l  Rings_CNZ_1	; 24 $0C
-	zoneOffsetTableEntry.l  Rings_CNZ_2	; 25
-	zoneOffsetTableEntry.l  Rings_CPZ_1	; 26 $0D
-	zoneOffsetTableEntry.l  Rings_CPZ_2	; 27
-	zoneOffsetTableEntry.l  Rings_DEZ_1	; 28 $0E
-	zoneOffsetTableEntry.l  Rings_DEZ_2	; 29
-	zoneOffsetTableEntry.l  Rings_ARZ_1	; 30 $0F
-	zoneOffsetTableEntry.l  Rings_ARZ_2	; 31
-	zoneOffsetTableEntry.l  Rings_SCZ_1	; 32 $10
-	zoneOffsetTableEntry.l  Rings_SCZ_2	; 33
+Off_Rings: zoneOrderedTable 4,2
+	zoneTableEntry.l  Rings_EHZ_1	; 0  $00
+	zoneTableEntry.l  Rings_EHZ_2	; 1
+	zoneTableEntry.l  Rings_Lev1_1	; 2  $01
+	zoneTableEntry.l  Rings_Lev1_2	; 3
+	zoneTableEntry.l  Rings_Lev2_1	; 4  $02
+	zoneTableEntry.l  Rings_Lev2_2	; 5
+	zoneTableEntry.l  Rings_Lev3_1	; 6  $03
+	zoneTableEntry.l  Rings_Lev3_2	; 7
+	zoneTableEntry.l  Rings_MTZ_1	; 8  $04
+	zoneTableEntry.l  Rings_MTZ_2	; 9
+	zoneTableEntry.l  Rings_MTZ_3	; 10 $05
+	zoneTableEntry.l  Rings_MTZ_4	; 11
+	zoneTableEntry.l  Rings_WFZ_1	; 12 $06
+	zoneTableEntry.l  Rings_WFZ_2	; 13
+	zoneTableEntry.l  Rings_HTZ_1	; 14 $07
+	zoneTableEntry.l  Rings_HTZ_2	; 15
+	zoneTableEntry.l  Rings_HPZ_1	; 16 $08
+	zoneTableEntry.l  Rings_HPZ_2	; 17
+	zoneTableEntry.l  Rings_Lev9_1	; 18 $09
+	zoneTableEntry.l  Rings_Lev9_2	; 19
+	zoneTableEntry.l  Rings_OOZ_1	; 20 $0A
+	zoneTableEntry.l  Rings_OOZ_2	; 21
+	zoneTableEntry.l  Rings_MCZ_1	; 22 $0B
+	zoneTableEntry.l  Rings_MCZ_2	; 23
+	zoneTableEntry.l  Rings_CNZ_1	; 24 $0C
+	zoneTableEntry.l  Rings_CNZ_2	; 25
+	zoneTableEntry.l  Rings_CPZ_1	; 26 $0D
+	zoneTableEntry.l  Rings_CPZ_2	; 27
+	zoneTableEntry.l  Rings_DEZ_1	; 28 $0E
+	zoneTableEntry.l  Rings_DEZ_2	; 29
+	zoneTableEntry.l  Rings_ARZ_1	; 30 $0F
+	zoneTableEntry.l  Rings_ARZ_2	; 31
+	zoneTableEntry.l  Rings_SCZ_1	; 32 $10
+	zoneTableEntry.l  Rings_SCZ_2	; 33
     zoneTableEnd
 
+	align $80
 Rings_EHZ_1:	BINCLUDE	"level/rings/EHZ_1.bin"
 Rings_EHZ_2:	BINCLUDE	"level/rings/EHZ_2.bin"
 Rings_Lev1_1:	BINCLUDE	"level/rings/01_1.bin"
@@ -88200,43 +88014,44 @@ Rings_SCZ_2:	BINCLUDE	"level/rings/SCZ_2.bin"
 ; --------------------------------------------------------------------------------------
 ; Offset index of object locations
 ; --------------------------------------------------------------------------------------
-Off_Objects: zoneOrderedOffsetTable 4,2
-	zoneOffsetTableEntry.l  Objects_EHZ_1	; 0  $00
-	zoneOffsetTableEntry.l  Objects_EHZ_2	; 1
-	zoneOffsetTableEntry.l  Objects_Null	; 2  $01
-	zoneOffsetTableEntry.l  Objects_Null	; 3
-	zoneOffsetTableEntry.l  Objects_Null	; 4  $02
-	zoneOffsetTableEntry.l  Objects_Null	; 5
-	zoneOffsetTableEntry.l  Objects_Null	; 6  $03
-	zoneOffsetTableEntry.l  Objects_Null	; 7
-	zoneOffsetTableEntry.l  Objects_MTZ_1	; 8  $04
-	zoneOffsetTableEntry.l  Objects_MTZ_2	; 9
-	zoneOffsetTableEntry.l  Objects_MTZ_3	; 10 $05
-	zoneOffsetTableEntry.l  Objects_MTZ_3	; 11
-	zoneOffsetTableEntry.l  Objects_WFZ_1	; 12 $06
-	zoneOffsetTableEntry.l  Objects_WFZ_2	; 13
-	zoneOffsetTableEntry.l  Objects_HTZ_1	; 14 $07
-	zoneOffsetTableEntry.l  Objects_HTZ_2	; 15
-	zoneOffsetTableEntry.l  Objects_HPZ_1	; 16 $08
-	zoneOffsetTableEntry.l  Objects_HPZ_2	; 17
-	zoneOffsetTableEntry.l  Objects_Null	; 18 $09
-	zoneOffsetTableEntry.l  Objects_Null	; 19
-	zoneOffsetTableEntry.l  Objects_OOZ_1	; 20 $0A
-	zoneOffsetTableEntry.l  Objects_OOZ_2	; 21
-	zoneOffsetTableEntry.l  Objects_MCZ_1	; 22 $0B
-	zoneOffsetTableEntry.l  Objects_MCZ_2	; 23
-	zoneOffsetTableEntry.l  Objects_CNZ_1	; 24 $0C
-	zoneOffsetTableEntry.l  Objects_CNZ_2	; 25
-	zoneOffsetTableEntry.l  Objects_CPZ_1	; 26 $0D
-	zoneOffsetTableEntry.l  Objects_CPZ_2	; 27
-	zoneOffsetTableEntry.l  Objects_DEZ_1	; 28 $0E
-	zoneOffsetTableEntry.l  Objects_DEZ_2	; 29
-	zoneOffsetTableEntry.l  Objects_ARZ_1	; 30 $0F
-	zoneOffsetTableEntry.l  Objects_ARZ_2	; 31
-	zoneOffsetTableEntry.l  Objects_SCZ_1	; 32 $10
-	zoneOffsetTableEntry.l  Objects_SCZ_2	; 33
+Off_Objects: zoneOrderedTable 4,2
+	zoneTableEntry.l  Objects_EHZ_1	; 0  $00
+	zoneTableEntry.l  Objects_EHZ_2	; 1
+	zoneTableEntry.l  Objects_Null	; 2  $01
+	zoneTableEntry.l  Objects_Null	; 3
+	zoneTableEntry.l  Objects_Null	; 4  $02
+	zoneTableEntry.l  Objects_Null	; 5
+	zoneTableEntry.l  Objects_Null	; 6  $03
+	zoneTableEntry.l  Objects_Null	; 7
+	zoneTableEntry.l  Objects_MTZ_1	; 8  $04
+	zoneTableEntry.l  Objects_MTZ_2	; 9
+	zoneTableEntry.l  Objects_MTZ_3	; 10 $05
+	zoneTableEntry.l  Objects_MTZ_3	; 11
+	zoneTableEntry.l  Objects_WFZ_1	; 12 $06
+	zoneTableEntry.l  Objects_WFZ_2	; 13
+	zoneTableEntry.l  Objects_HTZ_1	; 14 $07
+	zoneTableEntry.l  Objects_HTZ_2	; 15
+	zoneTableEntry.l  Objects_HPZ_1	; 16 $08
+	zoneTableEntry.l  Objects_HPZ_2	; 17
+	zoneTableEntry.l  Objects_Null	; 18 $09
+	zoneTableEntry.l  Objects_Null	; 19
+	zoneTableEntry.l  Objects_OOZ_1	; 20 $0A
+	zoneTableEntry.l  Objects_OOZ_2	; 21
+	zoneTableEntry.l  Objects_MCZ_1	; 22 $0B
+	zoneTableEntry.l  Objects_MCZ_2	; 23
+	zoneTableEntry.l  Objects_CNZ_1	; 24 $0C
+	zoneTableEntry.l  Objects_CNZ_2	; 25
+	zoneTableEntry.l  Objects_CPZ_1	; 26 $0D
+	zoneTableEntry.l  Objects_CPZ_2	; 27
+	zoneTableEntry.l  Objects_DEZ_1	; 28 $0E
+	zoneTableEntry.l  Objects_DEZ_2	; 29
+	zoneTableEntry.l  Objects_ARZ_1	; 30 $0F
+	zoneTableEntry.l  Objects_ARZ_2	; 31
+	zoneTableEntry.l  Objects_SCZ_1	; 32 $10
+	zoneTableEntry.l  Objects_SCZ_2	; 33
     zoneTableEnd
 
+	align $80
 	; These things act as boundaries for the object layout parser, so it doesn't read past the end/beginning of the file
 	ObjectLayoutBoundary
 Objects_EHZ_1:	BINCLUDE	"level/objects/EHZ_1.bin"
